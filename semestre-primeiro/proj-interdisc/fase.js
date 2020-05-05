4ad62cf4b0cf5a42608011af43e468ab57b94e2c
João Kitajima <joao.kitajima@outlook.com>
2020-05-05 16:12:54 -0300

    Update and rename humberto-mario.js to fase.js

--- semestre-primeiro/proj-interdisc/fase.js
+++ semestre-primeiro/proj-interdisc/fase.js
@@ -1,53 +1,48 @@
 var telas = ["menu"];
-var larguraJogo = 800;
-var alturaJogo = 600;
 
 var cenario;
 var camada;
 var boneco;
 var setas;
-var tecla_pulo;
 
 function menu() {
 	
 	this.preload = function () {		
 		game.stage.backgroundColor = "#6185F8";
+		game.load.tilemap('fase', 'fase.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('textura', 'supermario-textura.png');
 		game.load.spritesheet('estudante', 'estudante.png', 36, 48);
-		game.load.image('textura', 'TileMario.png');
-		game.load.tilemap('fase1', 'level1.json', null, Phaser.Tilemap.TILED_JSON);
-		game.load.spritesheet('estudante', 'estudante.png', 21, 29);
-		game.load.spritesheet('moeda', 'coin.png', 32, 32)
+		game.load.spritesheet('moeda', 'moeda.png', 32, 32)
 	};
 
 	
 	this.create = function () {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		cenario = game.add.tilemap('fase1');
+		
+		cenario = game.add.tilemap('fase');
 		cenario.addTilesetImage('textura');
 		cenario.setCollisionByExclusion([70, 80, 111, 315, 316, 317, 348, 349, 350, 513, 514, 515, 546, 547, 548, 669, 670, 671 ]);
-		boneco = game.add.sprite(400, 500, 'estudante');		
-		game.camera.follow(boneco);
-		camada = cenario.createLayer('Tile Layer 1');
+				
+		camada = cenario.createLayer('camada-primaria');
 		camada.resizeWorld();
 		
-		
+		boneco = game.add.sprite(32, 1400, 'estudante');
+		game.camera.follow(boneco);
 		
 		game.physics.arcade.enable(boneco, Phaser.Physics.ARCADE);
 		boneco.body.collideWorldBounds = true;
 		boneco.body.gravity.y = 500;
 		boneco.body.bounce.x = 0.15;
 		boneco.body.bounce.y = 0.3;
-		boneco.animations.add("mov_esquerda", [4, 3, 2, 1, 0], 6, true);
-		boneco.animations.add("mov_direita", [6, 7, 8, 9, 10], 6, true);
+		boneco.body.maxVelocity.x = 300;
+		boneco.body.drag.x = 500;
+		
 		boneco.animations.add("parado", [5], 1, true);
-		boneco.animations.add("pulando_esq", [7], 2, true);
-		boneco.body.maxVelocity.x = 500;
-		boneco.body.drag.x = 5000;
-		cenario.setCollisionByExclusion([70, 80, 111, 315, 316, 317, 348, 349, 350, 669, 670, 671 ]);
+		boneco.animations.add("mov_esquerda", [3, 2, 1, 0], 6, true);
+		boneco.animations.add("mov_direita", [7, 8, 9, 10], 6, true);
+		boneco.animations.add("pulando_dir", [8], 2, true);
 		
 		setas = game.input.keyboard.createCursorKeys();
-		tecla_pulo = setas.up;
-		tecla_pulo.onDown.add(pular);
 		
 		moedas = game.add.group();
 		moedas.enableBody = true;
@@ -58,14 +53,16 @@ function menu() {
 	};
 	
 		function criarMoeda(x, y) {
-		var moeda = game.add.sprite(x, y, 'moeda');
-		moedas.add(moeda);
-		game.physics.arcade.enable(moeda);			
-		moeda.animations.add("girando", [0, 1, 2, 3, 4, 5], 10, true);
-		moeda.animations.play("girando");
-		moeda.body.immovable = true;
-	}
-		function coletouMoeda(player, moeda) {
+			var moeda = game.add.sprite(x, y, 'moeda');
+			moedas.add(moeda);
+			
+			game.physics.arcade.enable(moeda);			
+			moeda.animations.add("girando", [0, 1, 2, 3, 4, 5], 10, true);
+			moeda.animations.play("girando");
+			moeda.body.immovable = true;
+		}
+		
+		function coletouMoeda(boneco, moeda) {
 			moeda.kill();
 		}
 	
@@ -85,21 +82,10 @@ function menu() {
 						boneco.animations.play("parado");
 						} 
 				}
-		if (setas.up.isDown && boneco.body.onFloor() || boneco.body.touching.down) {
-		boneco.body.velocity.y = -400;
-		if (setas.up.isDown) {
-			boneco.animations.play("pulando_esq")
-			}
-		}
-		
-	};
-	
-	function pular() {
 				
-		if (dude.body.onFloor() || dude.body.touching.down) {
-			dude.body.velocity.y = -500;
-		}
-		
-	}
+		if (setas.up.isDown && (boneco.body.onFloor() || boneco.body.touching.down)) {
+			boneco.body.velocity.y = -300;			
+		}				
+	};	
 	
 }

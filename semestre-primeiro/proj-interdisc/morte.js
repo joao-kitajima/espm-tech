dff023d365ae822072c54f206c51517681deb859
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2020-05-20 13:28:39 -0300

    Update nas telas e alguns ajustes (#10)
    * Update morte.js
    
    Aleatoriedade das frases e imagens do humberto, placar da tela configurado e reset na pontuação após passar pela tela.
    
    * Update menu.js
    
    Tela de menu pronta!
    
    * Add files via upload
    
    Biblioteca do Humberto e elementos para a tela de menu inicial.

--- semestre-primeiro/proj-interdisc/morte.js
+++ semestre-primeiro/proj-interdisc/morte.js
@@ -1,33 +1,71 @@
 function morte() {
 	
-	var telam;
-	var foto;
-	var frase;
+	var frases;
+	var imagens;
 	var estilo;
-	var vidaAtual = 3;
-	var vidas;
+	var vidaAtual;
 	
 	this.preload = function () {
 		
 		game.stage.backgroundColor = "#000000";
 		game.load.image("tmorte", "Tela-morte.jpg");
 		game.load.image("humberto", "humberto-1.jpg");
+		game.load.image("humberto2", "humberto-2.jpg");
+		game.load.image("humberto3", "humberto-3.jpg");
 		
 	};
 	
 	this.create = function () {
-		telam = game.add.image(0, 0, "tmorte");
-		foto = game.add.image(360,85, "humberto");
+		frases = [
+			"\"Ódio no coração!\"",
+			"\"Quem sabe se você\n endireitar as costas vai\n conseguir jogar melhor!\"",
+			"\"Cuidado com as palavras\n de baixo calão!\"",
+			"Não diga isso, diga:\n \"que desafiador!\"",
+			"\"Chegou cedo para o\n próximo semestre!\"",
+			"\"Então você quer dizer que\n a culpa é minha, é isso?\"",
+			"\"Total deselegante\"",
+			"\"Veja só que linguajar chulo!\"",
+			"\"O nome disso é preconceito\"",
+			"\"O que é uma pandemia\n perto da minha aula\"",
+			"\"Como assim sem reprovações?\n Para que eu dou aula então?\"",
+			"\"Vai nessa que vai dar\n super certo.\"",
+			"\"Fim de carreira,\n Sr. Jogador.\"",
+			"\"A honestidade vale mais\n do que o conhecimento\"",
+			"\"(Colaram na prova?)\"\n\"Os inocentes irão pagar\n hoje também\"",
+		];
+		imagens = [
+			"humberto",
+			"humberto2",
+			"humberto3",
+		];
+		//------ Aleatorização das frases e imagens -------
+		var vez, i, total = frases.length;
+		for (vez = 0; vez < 4; vez++) {
+			for (i = 0; i < total; i++) {
+				frases.push(frases[i]);
+			}
+		}		
+		total = imagens.length;
+		for (vez = 0; vez < 4; vez++) {
+			for (i = 0; i < total; i++) {
+				imagens.push(imagens[i]);
+			}
+		}
+		//------------------------------------------
+		var x = (Math.random() * imagens.length) | 0;
+		game.add.image(0, 0, "tmorte");
+		game.add.image(360,85, imagens[x]);
 		
-	var estilo = {
+		var estilo = {
 			font: "normal 16px 'Press Start 2P'",
 			fill: "#ffffff",
 			fontSize: "60px"
 		};
+		//------------------------------------------
+		x = (Math.random() * frases.length) | 0;
+		game.add.text(50, 500, frases[x], estilo);
 		
-		frase = game.add.text(150, 500, "\"Ódio no coração\"", estilo);
-		
-		mostraVida();
+		mostraPlacar();
 		setTimeout(Fade, 3000);
 		fadeIn();
 	};
@@ -36,25 +74,26 @@ function morte() {
 		
 	};
 	
-	function Fade() {
-		
-		fadeOut(fadeOutAcabou);
-	}
-	
-	function fadeOutAcabou() {
-		
-		game.state.start("menu");
-		
-	}
-	
-	function mostraVida() {
+	function mostraPlacar() {
 		estilo = {
 			font: "normal 16px 'Press Start 2P'",
 			fill: "#ffffff",
 			fontSize: "45px"
 		};
 		
-		vidas = game.add.text(200, 243, vidaAtual, estilo);
+		game.add.text(200, 96, pontuacao, estilo);
+		pontuacao = 0;
+		game.add.text(200, 243, vidaAtual, estilo);
 		vidaAtual--;
 	}
+	
+	function Fade() {
+		fadeOut(fadeOutAcabou);
+	}
+	
+	function fadeOutAcabou() {
+		game.state.start("fase");
+	}
+	
+
 }
